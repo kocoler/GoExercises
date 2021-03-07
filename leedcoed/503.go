60bf571bbe88f9dc65ce4a8158b68e51c7e68065
zhang <tsglsdrs@163.com>
2021-03-07 20:12:39 +0800

    update

--- leedcoed/503.go
+++ leedcoed/503.go
@@ -1,1 +1,69 @@
 package main
+
+import "fmt"
+
+type stack struct {
+	val []int
+	num int
+}
+
+func (s *stack) pop() int {
+	res := s.val[s.num-1]
+	s.val = s.val[:s.num-1]
+	s.num --
+	return res
+}
+
+func (s *stack) push(i int) {
+	s.val = append(s.val, i)
+	s.num++
+}
+
+func (s *stack) top() int {
+	return s.val[s.num-1]
+}
+
+func (s *stack) length() int {
+	return s.num
+}
+
+func main() {
+	fmt.Println(nextGreaterElements([]int{1, 2, 1}))
+}
+// 1 2 1 1 2 1
+func nextGreaterElements(nums []int) []int {
+	var s stack
+	temp := append(nums, nums ...)
+	n := len(temp)
+	res := make([]int, n)
+	for i := n-1; i >= 0; i -- {
+		fmt.Println(s.val, res)
+		if s.length() > 0 {
+			if s.top() > temp[i] {
+				res[i] = s.top()
+				s.push(temp[i])
+			} else {
+				var flag bool
+				for s.length() > 0 {
+					if s.top() > temp[i] {
+						res[i] = s.top()
+						s.push(temp[i])
+						flag = true
+						break
+					} else {
+						s.pop()
+					}
+				}
+				if !flag {
+					s.push(temp[i])
+					res[i] = -1
+				}
+			}
+		} else {
+			s.push(temp[i])
+			res[i] = -1
+		}
+	}
+
+	return res[:n/2]
+}
