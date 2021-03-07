f740795a7a931dd9900da284718997f26e08e056
zhang <tsglsdrs@163.com>
2021-03-07 20:11:26 +0800

    update

--- leedcoed/125.go
+++ leedcoed/125.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(isPalindrome("1p2"))
+}
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	lens := len(s)
+	left := 0
+	right := lens - 1
+	for left < right {
+		for !isAlpha(s[left]) && left < right {
+			left ++
+		}
+		for !isAlpha(s[right]) && left < right {
+			right --
+		}
+
+		if s[left] != s[right] {
+			return false
+		}
+
+		left ++
+		right --
+	}
+
+	return true
+}
+
+func isAlpha(b byte) bool {
+	if 'a' <= b && b <= 'z' {
+		return true
+	}
+	if '0' <= b && b <= '9' {
+		return true
+	}
+
+	return false
+}
