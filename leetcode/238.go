97265973bbdfaaf1aec42c2d6af19c27084e9e68
zhang <tsglsdrs@163.com>
2021-05-25 09:13:31 +0800

    updates

---
+++ leetcode/238.go
@@ -0,0 +1,21 @@
+package main
+
+// 除自身以外数组的乘积
+// 优化到时间复杂度 O(1) 的办法: 把答案数组当作 dp 数组来用
+var dp [100001][2]int
+func productExceptSelf(nums []int) []int {
+	lenn := len(nums)
+	dp = [100001][2]int{}
+	res := make([]int, lenn)
+	dp[0][0] = 1
+	dp[lenn-1][1] = 1
+	for i := 1; i < lenn; i ++ {
+		dp[i][0] = dp[i-1][0] * nums[i-1]
+	}
+	for i := lenn-2; i >= 0; i -- {
+		dp[i][1] = dp[i+1][1] * nums[i+1]
+		res[i] = dp[i][0] * dp[i][1]
+	}
+	res[lenn-1] = dp[lenn-1][0] * dp[lenn-1][1]
+	return res
+}
