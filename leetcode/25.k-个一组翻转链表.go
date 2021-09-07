c8b662469ecde3a575e5efe5708dfb9ed0a68e83
zhang <tsglsdrs@163.com>
2021-09-07 22:36:23 +0800

    25

---
+++ leetcode/25.k-个一组翻转链表.go
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=golang
+ *
+ * [25] K 个一组翻转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-nodes-in-k-group/description/
+ *
+ * algorithms
+ * Hard (65.65%)
+ * Likes:    1285
+ * Dislikes: 0
+ * Total Accepted:    221.7K
+ * Total Submissions: 337.7K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * 
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * 
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 
+ * 进阶：
+ * 
+ * 
+ * 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], k = 1
+ * 输出：[1,2,3,4,5]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：head = [1], k = 1
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 列表中节点的数量在范围 sz 内
+ * 1 
+ * 0 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 7:04
+var num int
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	num = k
+
+	if head == nil {
+		return nil 
+	}
+
+	return reverse(head)
+}
+
+func reverse(head *ListNode) *ListNode {
+    // fmt.Println(head.Val)
+	h := head
+	n := head
+	for i := 0; i < num; i ++ {
+		if n == nil {
+			return h
+		}
+		n = n.Next
+	}
+	nx := n
+
+	var next *ListNode
+	tail := h
+	for i := 1; i < num; i ++ {
+		next = h.Next
+		h.Next, next.Next = next.Next, tail
+        tail = next
+	}
+
+	h.Next = reverse(nx)
+
+	return tail
+}
+// @lc code=end
+
